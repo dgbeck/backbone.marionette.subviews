f1f54461dadc2068d1bfa0467b15d9033ccc1a28
David Beck <david@rotundasoftware.com>
2012-12-16 10:16:13 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Backbone.Marionette.Subviews
+
+Easily create subviews (views within views) in your Backbone.js Marionette apps, directly from your underscore templates. This is a drop-in plugin that augments the original Marionette ItemView class with additional functionality.
+
+The work on this plugin was inspired by [Spike Brehm's](https://github.com/spikebrehm) NestedView.
+
+## Benefits
+
+* Create named subviews with a crystal clear syntax directly from your underscore templates 
+* Subviews are created with pure javascript - no need to put "placeholder" elements in your templates
+* Access subviews via the automatically populated myView.subviews hash.
+* Organize all javascript logic for creating subviews in a declarative hash
+
+## Usage
+
+In your underscore template for MyItemView, which contains a subview MySubview:
+
+```html
+<script type='text/template' id="MyItemViewTemplate">
+	<h1>This is my item view template</h1>
+
+	<%= subview.( "MySubview" ) %>
+</script>
+```
+
+Now in MyItemView.js:
+
+```javascript
+MyItemViewClass = Backbone.Marionette.ItemView.extend( {
+	template: "#MyItemViewTemplate",
+
+	subviewCreators : {
+		"MySubview" : function() {
+			var options = {};
+			// do any logic required to create initilaization options
+			new MySubviewClass( options );
+		}
+	}
+
+	onAfterRender : {
+		// anytime after rendering, you can find the subviews in this.subviews
+		this.subviews[ "MySubview ].bind( "click", _.mySubview_onClick, this );
+	}
+} );
+```
+
+## Details
+
+The `subview` helper will automatically be available in all of your underscore templates. The helper takes one argument, which is the name of the subview to be created.
+
+The subviewCreators hash of your view should contain an element for each subview. The key of each element is the subview's name, and the value is a function that returns the subview instance.
+
+Subviews are not rendered after the parent view has completely finished rendering. A new "after:render" event is fired (and its cooresponding onAfterRender function is called) after all subviews have finished rendering. The sequence of events is as follows:
+
+	1. before:render event is fired
+	2. [parentview is rendered]
+	3. render event is fired
+	4. [subviews are created and rendered in the order they appear in the underscore template]
+	5. after:render event is fired
+
+
---
+++ backbone.marionette.subviews.js
@@ -0,0 +1,85 @@
+/*!
+ * Backbone.Marionette.Subviews, v0.1
+ * Copyright (c)2012 David Beck, Rotunda Software, LLC.
+ * Distributed under MIT license
+ * http://github.com/dgbeck/backbone.marionette.subviews
+*/
+(function(){
+	var debugMode = true;
+
+	var originalMarionetteItemView = Backbone.Marionette.ItemView;
+
+	Backbone.Marionette.ItemView = originalMarionetteItemView.extend({
+ 
+		createSubviewPlaceholder : function( subviewName ) {
+			var subviewCreator = this.subviewCreators[ subviewName ];
+			if( _.isUndefined( subviewCreator ) ) throw "Attempt to create undefined subview: " + subviewName;
+
+			return "<script type='text/template' class='view-placeholder' data-view-name='" + subviewName + "'></script>";
+		},
+
+		// override Marionette's standard mixinTemplateHelpers function so that we can always mix in our
+		// "subview" helper in addition to any helpers specified in the templateHelpers element.
+		mixinTemplateHelpers: function( target ) {
+			var _this = this;
+			
+			// first extend with our subview template helper
+			target.subview = function(){ return _this.createSubviewPlaceholder.apply( _this, arguments ); };
+
+			// now call default behavior to mix in helpers specified in the templateHelpers element
+			return originalMarionetteItemView.prototype.mixinTemplateHelpers.apply( this, arguments );
+		},
+
+		render : function() {
+			var _this = this;
+
+			if( debugMode )
+			{
+				console.group( "Rendering view" );
+				console.log( this );
+			}
+	
+			this.subviews = {};
+
+			originalMarionetteItemView.prototype.render.apply( this, arguments );
+
+			this.$el.addClass( "view" );
+			this.$el.data( "view", this );
+
+			if( this.conditionalClassNames )
+				_.each( this.conditionalClassNames, function( conditionalFunction, className ) {
+					if( conditionalFunction.apply( _this ) ) _this.$el.addClass( className );
+				} );
+
+			this.$( "script.view-placeholder" ).each( function() {
+				var thisPlaceHolderScriptEl = $( this );
+				var subviewName = thisPlaceHolderScriptEl.attr( "data-view-name" );
+				var subviewCreator = _this.subviewCreators[ subviewName ];
+
+				if( _.isUndefined( subviewCreator ) ) throw "Can not find subview creator that corresponds to placeholder: " + subviewName;
+
+				if( ! _.isUndefined( _this.subviews[ subviewName ] ) )
+					throw( "There is already a subview with the name \"" + subviewName + "\". Each subview must have a unique name." );
+
+				if( debugMode ) console.log( "Creating subview " + subviewName );
+				var newView = subviewCreator.apply( _this );
+
+				_this.subviews[ subviewName ] = newView;
+
+				thisPlaceHolderScriptEl.replaceWith( newView.$el );
+			} );
+
+			_.each( this.subviews, function( thisSubview, subviewName ) {
+				if( debugMode ) console.group( "Rendering subview " + subviewName );
+				thisSubview.render();
+				if( debugMode ) console.groupEnd();
+			} );
+
+			this.triggerMethod("after:render", this);
+
+			if( debugMode ) console.groupEnd(); // "Rendering view"
+
+			return this;
+		}
+	});
+})();
